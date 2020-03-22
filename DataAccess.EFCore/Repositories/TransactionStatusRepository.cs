2b88caa32120a1783af665ae48ef5ac76196a071
Mykola Demii <mykola.demii@gmail.com>
2020-03-22 18:51:47 +0200

    Updated TransactionStatusRepository to use dbContext

--- DataAccess.EFCore/Repositories/TransactionStatusRepository.cs
+++ DataAccess.EFCore/Repositories/TransactionStatusRepository.cs
@@ -1,4 +1,6 @@
-﻿using DataAccess.Models;
+﻿using AutoMapper;
+using DataAccess.EFCore.DbContexts;
+using DataAccess.Models;
 using DataAccess.Parameters;
 using DataAccess.Repositories;
 using System;
@@ -9,6 +11,15 @@ namespace DataAccess.EFCore.Repositories
 {
     public class TransactionStatusRepository : ITransactionStatusRepository
     {
+        private readonly IMapper m_mapper;
+        private TransactionDbContext m_dbContext;
+
+        public TransactionStatusRepository(TransactionDbContext dbContext, IMapper mapper)
+        {
+            m_mapper = mapper;
+            m_dbContext = dbContext;
+        }
+
         public void Create(TransactionStatusModel item)
         {
             throw new NotImplementedException();
@@ -26,29 +37,9 @@ namespace DataAccess.EFCore.Repositories
 
         public Task<IEnumerable<TransactionStatusModel>> GetAllAsync(ParametersBase parameters = null)
         {
-            var statusList = new List<TransactionStatusModel>()
-            {
-                new TransactionStatusModel
-                {
-                    Id=1,
-                    Name="Approved",
-                    ShortName="A",
-                },
-                new TransactionStatusModel
-                {
-                    Id=2,
-                    Name="Rejected",
-                    ShortName="R",
-                },
-                new TransactionStatusModel
-                {
-                    Id=3,
-                    Name="Done",
-                    ShortName="D",
-                },
-            };
-
-            return Task<IEnumerable<TransactionStatusModel>>.Factory.StartNew(() => statusList);
+            var statusEntities = m_dbContext.TransactionStatuses.AsQueryable();
+            var statusModels = m_mapper.Map<IEnumerable<TransactionStatusModel>>(statusEntities);
+            return Task<IEnumerable<TransactionStatusModel>>.Factory.StartNew(() => statusModels);
         }
 
         public void Update(TransactionStatusModel item)
---
+++ TransactionWebApi/Automapper/Profiles/TransactionStatusProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DataAccess.EFCore.Entities;
+using DataAccess.Models;
+
+namespace TransactionWebApi.Automapper.Profiles
+{
+    public class TransactionStatusProfile : Profile
+    {
+        public TransactionStatusProfile()
+        {
+            CreateMap<TransactionStatusEntity, TransactionStatusModel>();
+        }
+    }
+}
